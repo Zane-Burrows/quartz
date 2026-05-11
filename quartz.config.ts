e1a724fa8a784d261022f25d1f3033714ee21c4b
Zane Burrows <zxburrows@gmail.com>
2026-05-11 15:05:34 -0700

    Quartz sync: May 11, 2026, 3:05 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -29,15 +29,15 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
+          light: "#efeee9",
+          lightgray: "#ddd8d2",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#fff23688",
+          secondary: "#800000",
+          tertiary: "#a64545",
+          highlight: "rgba(128, 0, 0, 0.10)",
+          textHighlight: "#80000022",
         },
         darkMode: {
           light: "#161618",
