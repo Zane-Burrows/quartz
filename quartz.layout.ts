a9666be465798c8c19abd1a44a15592b9157418b
Zane Burrows <zxburrows@gmail.com>
2026-05-18 09:39:39 -0700

    Quartz sync: May 18, 2026, 9:39 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [Component.Backlinks()],
+  afterBody: [],
   footer: Component.Footer(),
 }
 
@@ -38,6 +38,7 @@ export const defaultContentPageLayout: PageLayout = {
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
+    Component.Backlinks(),
   ],
 }
 
