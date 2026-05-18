4ff86a3483fbd13aa2824aabaaf858e98acb3942
Zane Burrows <zxburrows@gmail.com>
2026-05-18 09:47:14 -0700

    Quartz sync: May 18, 2026, 9:47 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://burrowsnotes.netlify.app",
+    baseUrl: "burrowsnotes.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
